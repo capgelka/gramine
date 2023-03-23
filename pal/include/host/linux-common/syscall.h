b0678392660a4490a907559d398e6c9aa8f39177
Leonid Dyachkov <ldyach@yandex.ru>
2023-03-23 19:20:00 +0200

    debug

--- pal/include/host/linux-common/syscall.h
+++ pal/include/host/linux-common/syscall.h
@@ -4,7 +4,12 @@
 #include <stdint.h>
 #include <stdnoreturn.h>
 
+#define FUZZ 1
+
 long do_syscall(long nr, ...);
+
+long do_syscall_wrapped(long nr, int num_args, ...);
+
 long clone(int (*f)(void*), void* stack, int flags, void* arg, void* parent_tid, void* tls,
            void* child_tid, void (*exit_func)(int));
 long vfork(void) __attribute__((returns_twice));
@@ -28,8 +33,36 @@ noreturn void _PalThreadExit_asm_stub(uint32_t* thread_stack_spinlock, int* clea
 
 #define PICK_SYSCALL_MACRO(name, _1, _2, _3, _4, _5, _6, NARGS, ...) DO_SYSCALL_##NARGS
 
+#ifdef FUZZ
+
+#define DO_SYSCALL_WRAPPED_0(name) do_syscall_wrapped(name, 0)
+#define DO_SYSCALL_WRAPPED_1(name, a1) do_syscall_wrapped(name, 1, _extend(a1))
+#define DO_SYSCALL_WRAPPED_2(name, a1, a2) do_syscall_wrapped(name, 2, _extend(a1), _extend(a2))
+#define DO_SYSCALL_WRAPPED_3(name, a1, a2, a3) do_syscall_wrapped(name, 3, _extend(a1), _extend(a2), _extend(a3))
+#define DO_SYSCALL_WRAPPED_4(name, a1, a2, a3, a4) \
+    do_syscall_wrapped(name, 4, _extend(a1), _extend(a2), _extend(a3), _extend(a4))
+#define DO_SYSCALL_WRAPPED_5(name, a1, a2, a3, a4, a5) \
+    do_syscall_wrapped(name, 5, _extend(a1), _extend(a2), _extend(a3), _extend(a4), _extend(a5))
+#define DO_SYSCALL_WRAPPED_6(name, a1, a2, a3, a4, a5, a6) \
+    do_syscall_wrapped(name, 6, _extend(a1), _extend(a2), _extend(a3), _extend(a4), _extend(a5), _extend(a6))
+
+#define PICK_SYSCALL_MACRO_WRAPPED(name, _1, _2, _3, _4, _5, _6, NARGS, ...)\
+        DO_SYSCALL_WRAPPED_##NARGS
+
+#define DO_SYSCALL(name, args...) \
+    PICK_SYSCALL_MACRO_WRAPPED(name, ##args, 6, 5, 4, 3, 2, 1, 0)(__NR_##name, ##args)
+
+#define DO_SYSCALL_ORIG_BY_NUM(name, args...) \
+    PICK_SYSCALL_MACRO(name, ##args, 6, 5, 4, 3, 2, 1, 0)(name, ##args)
+
+#define DO_SYSCALL_ORIG(name, args...) \
+    PICK_SYSCALL_MACRO(name, ##args, 6, 5, 4, 3, 2, 1, 0)(__NR_##name, ##args)
+#else
+
 #define DO_SYSCALL(name, args...) \
     PICK_SYSCALL_MACRO(name, ##args, 6, 5, 4, 3, 2, 1, 0)(__NR_##name, ##args)
+
+#endif
 
 #define IS_PTR_ERR(val) ((unsigned long)(val) >= -4095UL)
 #define PTR_TO_ERR(val) ((long)(val))
