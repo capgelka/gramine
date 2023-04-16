9e583fcfdec60520a66a864bc6e15e89fd3e5209
Leonid Dyachkov <ldyach@yandex.ru>
2023-04-16 18:47:29 +0300

    temporal solution to fix hanging on exit

--- libos/src/ipc/libos_ipc_worker.c
+++ libos/src/ipc/libos_ipc_worker.c
@@ -409,11 +409,11 @@ int init_ipc_worker(void) {
 }
 
 void terminate_ipc_worker(void) {
-    set_pollable_event(&g_worker_thread->pollable_event);
+    // set_pollable_event(&g_worker_thread->pollable_event);
 
-    while (__atomic_load_n(&g_clear_on_worker_exit, __ATOMIC_ACQUIRE)) {
-        CPU_RELAX();
-    }
+    // while (__atomic_load_n(&g_clear_on_worker_exit, __ATOMIC_ACQUIRE)) {
+    //     CPU_RELAX();
+    // }
 
     put_thread(g_worker_thread);
     g_worker_thread = NULL;
